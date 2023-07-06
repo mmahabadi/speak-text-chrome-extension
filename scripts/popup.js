3d322fd1c89dacf302a2ffe8841450eba3d6bfa6
Mohsen <mohsen.mahabadi@rabobank.nl>
2023-07-06 14:43:42 +0200

    add setting to the popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   const playButton = document.getElementById("play-button");
+  const speedInput = document.getElementById("speed");
+
+  (function () {
+    chrome.storage.sync.get("speed", function (data) {
+      if (data.speed) {
+        speedInput.value = data.speed;
+      }
+    });
+  })();
+
   playButton.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.storage.sync.get("speed", function (data) {
@@ -10,6 +20,13 @@ document.addEventListener("DOMContentLoaded", function () {
           args: [speed],
         });
       });
+    });
+  });
+
+  speedInput.addEventListener("change", function () {
+    const speed = speedInput.value;
+    chrome.storage.sync.set({ speed }, function () {
+      console.log("Speed saved: " + speed);
     });
   });
 });
